40b9344f94bf4d3e2e68ab2c8989f2045dc9ad23
den4xss <131610958+den4xss@users.noreply.github.com>
2023-04-23 20:24:14 +0300

    Add File

---
+++ CreatePrimitives.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePrimitives : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
